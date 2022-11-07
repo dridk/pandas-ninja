6ba31329d9cc9be33feacc4a714e5f854d9ca44d
dridk <sacha@labsquare.org>
2022-11-07 23:27:13 +0100

    Missing file

---
+++ src/Grid.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+
+import { Split, DefaultSplitter } from "@geoffcox/svelte-splitter"
+
+let splitter_size = "2px";
+let splitter_color = "gray";
+let splitter_active_color = "blue";
+
+</script>
+
+
+
+<div class="wrapper">
+
+	<Split splitterSize={splitter_size}>
+
+		<svelte:fragment slot="primary">
+			<Split horizontal splitterSize={splitter_size} >
+				<div slot="primary" class="h-full">
+
+					<!-- First cell  -->
+
+					<slot name="a"></slot>
+
+				</div>
+
+
+				<svelte:fragment slot="splitter">
+					<DefaultSplitter color={splitter_color} hoverColor={splitter_active_color} dragColor={splitter_active_color} />
+				</svelte:fragment>
+
+
+				<div slot="secondary" class="h-full">
+
+					<!-- Second cell  -->
+
+					<slot name="b"></slot>
+
+				</div>
+			</Split>
+		</svelte:fragment>  
+
+		<svelte:fragment slot="splitter">
+			<DefaultSplitter color={splitter_color} hoverColor={splitter_active_color} dragColor={splitter_active_color} />
+		</svelte:fragment>
+
+		<svelte:fragment slot="secondary">
+			<Split horizontal splitterSize={splitter_size} >
+				<div slot="primary" class="h-full">
+
+					<!-- Third cell  -->
+
+					<slot name="c"></slot>
+
+
+				</div>
+
+				<svelte:fragment slot="splitter">
+					<DefaultSplitter color={splitter_color} hoverColor={splitter_active_color} dragColor={splitter_active_color} />
+				</svelte:fragment>
+
+				<div slot="secondary" class="h-full">
+
+					<!-- Fourth cell  -->
+
+					<slot name="d"></slot>
+
+				</div>
+			</Split>
+		</svelte:fragment>
+
+	</Split>
+
+
+
+
+</div>
+
+
+<style>
+	.wrapper {
+
+		height: 90vh;
+
+	}
+</style>
---
+++ src/LogBox.svelte
@@ -0,0 +1,7 @@
+
+
+
+<div class="tabs tabs-boxed">
+  <a class="tab">Console</a> 
+  <a class="tab tab-active">Error</a> 
+</div>
