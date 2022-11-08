0aa540b246f5daed258d8cbfe1b95fb8a661deba
Sacha Schutz <sacha@labsquare.org>
2022-11-08 22:45:52 +0100

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "pandas-ninja/",
+  base: "/pandas-ninja/",
 	plugins: [svelte()]
 })
